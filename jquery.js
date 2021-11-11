8fbab3633cda876647192aa134570d6cf3a7e139
franklynmarty0220 <64612023+franklynmarty0220@users.noreply.github.com>
2021-11-11 16:11:48 -0500

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    console.log("Let's get ready to party with jQuery!");
+})
+
+$("article").addClass(".image-center");
+
+$("article").last().remove('p')
+
+$('#title').css("font-size", "30px");
